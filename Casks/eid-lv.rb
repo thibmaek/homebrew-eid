e1709536454370e68b259338717979e9a4e67d65
Vítor Galvão <info@vitorgalvao.com>
2015-10-21 19:13:23 +0100

    added eid-lv

---
+++ Casks/eid-lv.rb
@@ -0,0 +1,19 @@
+cask :v1 => 'eparakstitajs' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.eparaksts.lv/files/ep3/eparakstitajs3-distribution.pkg'
+  name 'eParakstītājs'
+  name 'Electronic identity card software for Latvia'
+  name 'eID Latvia'
+  homepage 'https://www.eparaksts.lv'
+  license :gratis
+
+  pkg 'eparakstitajs3-distribution.pkg'
+
+  uninstall :pkgutil => [
+                         'com.gemalto.classicclient.pkg',
+			 'lv.euso.signanywhere.pkg',
+			 'org.latvia-eid.mac'
+			]
+end
